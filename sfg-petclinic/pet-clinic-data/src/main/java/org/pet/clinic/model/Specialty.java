1a9e8f4f8f3984685a5ad0f2d8ad26d7b8ce30f4
LENOVO <LENOVO@host.docker.internal>
2021-03-07 16:05:10 +0530

    closes #28

---
+++ sfg-petclinic/pet-clinic-data/src/main/java/org/pet/clinic/model/Specialty.java
@@ -0,0 +1,14 @@
+package org.pet.clinic.model;
+
+public class Specialty extends BaseEntity {
+	public String description;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
