50a005c4c06536ea81a12254a4b6f26fb584c1fe
LENOVO <LENOVO@host.docker.internal>
2021-03-07 16:23:18 +0530

    Closes #30

---
+++ sfg-petclinic/pet-clinic-data/src/main/java/org/pet/clinic/services/map/PetTypeMapService.java
@@ -0,0 +1,40 @@
+package org.pet.clinic.services.map;
+
+import java.util.Set;
+
+import org.pet.clinic.model.PetType;
+import org.pet.clinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+	@Override
+	public PetType save(PetType obj) {
+		// TODO Auto-generated method stub
+		return super.save(obj);
+	}
+
+	@Override
+	public void delete(PetType obj) {
+		// TODO Auto-generated method stub
+		super.delete(obj);
+	}
+
+	@Override
+	public Set<PetType> findAll() {
+		// TODO Auto-generated method stub
+		return super.findAll();
+	}
+
+	@Override
+	public PetType findById(Long id) {
+		// TODO Auto-generated method stub
+		return super.findById(id);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		super.findById(id);
+	}
+}
