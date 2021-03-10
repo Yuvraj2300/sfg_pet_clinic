bd9f9647792694172bf2ec01d239a9a253f5b9c4
LENOVO <LENOVO@DESKTOP-5J6GSB7.mshome.net>
2021-03-10 22:06:43 +0530

    Added JPA entities for Vet's data model

--- sfg-petclinic/pet-clinic-data/src/main/java/org/pet/clinic/model/Specialty.java
+++ sfg-petclinic/pet-clinic-data/src/main/java/org/pet/clinic/model/Specialty.java
@@ -1,6 +1,13 @@
 package org.pet.clinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity {
+	@Column(name = "description")
 	public String description;
 
 	public String getDescription() {
